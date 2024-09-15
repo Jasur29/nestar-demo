aff12737f7eff67d4967584f391d531f35c342e1
Jasur29 <kaharov2017@gmail.com>
2024-09-15 21:06:23 +0900

    feat:integration Header and Footer via Layouts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT -PAGE ROUTER");
   return (
     <Html lang="en">
       <Head>
--- pages/property/[productId]/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"; // bu hook useRouter
-
-const PropertyDetail = () => {
-  const router = useRouter();
-  const { productId } = router.query;
-  return <div>PROPERTY DETAIL{productId}</div>;
-};
-export default PropertyDetail;
