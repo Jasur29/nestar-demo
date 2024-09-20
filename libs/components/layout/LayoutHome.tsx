b94a9e477e251d7caad2dc601a02e18402252713
Jasur29 <kaharov2017@gmail.com>
2024-09-20 16:32:57 +0900

    feat:publishing main layout components

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,23 +1,37 @@
 import { Stack } from "@mui/material";
 import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
+import HeaderFilter from "../homepage/HeaderFilter";
 
 const withLayoutMain = (Component: any) => {
-  return (props: any) => {
-    return (
-      <>
-        <Head>
-          <title>Nestar</title>
-        </Head>
-        <Stack id="pc-wrap">
-          <Stack sx={{ background: "#81c784" }}>Header Home</Stack>
+  return (props: any) => (
+    <>
+      <Head>
+        <title>Nestar</title>
+      </Head>
+
+      <Stack id="pc-wrap">
+        <Stack id="top">
+          <Top />
+        </Stack>
+
+        <Stack className="header-main">
+          <Stack className="container">
+            <HeaderFilter />
+          </Stack>
+
           <Stack id="main">
             <Component {...props} />
           </Stack>
-          <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+
+          <Stack id="footer">
+            <Footer />
+          </Stack>
         </Stack>
-      </>
-    );
-  };
+      </Stack>
+    </>
+  );
 };
 
 export default withLayoutMain;
